3854bb9ebebd63b564b03dc36552258cd9da611c
leo <leo.c.maden@gmail.com>
2020-04-26 13:55:54 +0100

    Added googletest

--- MathL.Tests/src/Main.cpp
+++ MathL.Tests/src/Main.cpp
@@ -1,44 +1,27 @@
 #include <iostream>
 
-#include "Matrix.h"
-#include "Vector.h"
+#include <gtest/gtest.h>
 
-namespace MathL {
 
-	typedef MathL::Matrix<2, 2, float> Mat2;
-	typedef MathL::Matrix<3, 3, float> Mat3;
-	typedef MathL::Matrix<4, 4, float> Mat4;
+int main(int argc, char** argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    std::cout << "gtest!" << std::endl;
+
+    int status = RUN_ALL_TESTS();
+
+    std::cin.get();
+
+    return status;
 }
 
-int main()
+TEST(gtestTest, Works)
 {
-	/*MathL::Mat3 mat;
-	auto mat2 = MathL::Mat3::Identity();
+    ASSERT_TRUE(false, "gtest doesnt work!");
+}
 
-	MathL::Vector<3, float> vec(1.0f, 2.0f, 3.0f);
-
-	std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;*/
-
-	// TODO: Implement transpose for matrix subclass.
-	//MathL::Matrix<3, 2, int> mat(nums);
-	//MathL::Matrix<2, 3, int> matT = mat.Transpose();
-
-	int nums[3][2]{ 1, 2, 3, 4, 5, 6 };
-	MathL::Matrix<3, 2, int> mat(nums);
-	MathL::MatrixBase<2, 3, int> matT = mat.Transpose();
-
-	MathL::Mat4 translation = MathL::Mat4::Translation({ 1.0f, 2.0f, 3.0f });
-	//MathL::Mat4 identity = MathL::Mat4::Identity();
-	//MathL::MatrixBase<4, 4, float> x = identity * translation;
-
-	//float a[2][2]{ 2, 3, 1, 1 };
-	//float b[2][2]{ 4, 1, 0, 1 };
-	//MathL::Mat2 mat1(a);
-	//MathL::Mat2 mat2(b);
-	//auto result = mat1 * mat2;
-
-	//MathL::Mat2 aMat(a);
-
-	std::cin.get();
-	return 0;
+TEST(gtestTest, Other)
+{
+    ASSERT_TRUE(true, "gtest doesnt work!");
 }
--- premake5.lua
+++ premake5.lua
@@ -48,15 +48,18 @@ project "MathL.Tests"
 
 	files {
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/googletest-1.10"
 	}
 
 	includedirs {
-		"MathL/src"
+		"MathL/src",
+		"MathL.Tests/vendor/googletest-1.10/googletest/include"
 	}
 
 	links {
-		"MathL"
+		"MathL",
+		"googletest"
 	}
 
 	filter "system:windows" 
@@ -71,3 +74,43 @@ project "MathL.Tests"
 	filter "configurations:Release"
 		defines "Release"
 		optimize "On"
+
+
+project "googletest"
+	location "MathL.Tests"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")	
+	objdir ("build/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"MathL.Tests/vendor/googletest-1.10/googletest/src/**.h",
+		"MathL.Tests/vendor/googletest-1.10/googletest/src/**.cc",
+		"MathL.Tests/vendor/googletest-1.10/googletest/include/**.h"
+	}
+
+	includedirs {
+		"MathL.Tests/vendor/googletest-1.10/googletest/include",
+		"MathL.Tests/vendor/googletest-1.10/googletest"--,
+		--"MathL.Tests/vendor/googletest-1.10/googlemock/include",
+		--"MathL.Tests/vendor/googletest-1.10/googlemock"
+	}
+
+	filter "system:windows" 
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+	
+	filter "configurations:Debug"
+		defines "DEBUG" 
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "Release"
+		optimize "On"
+
+	buildoptions {
+		"/bigobj"
+	}
+	
